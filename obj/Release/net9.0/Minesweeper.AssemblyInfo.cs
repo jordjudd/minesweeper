7dbcbc56a23a913383e6cac11bcf00235893ee83
Jordan Judd <jordjudd@amazon.com>
2025-07-24 12:36:43 -0500

    comment

--- obj/Release/net9.0/Minesweeper.AssemblyInfo.cs
+++ obj/Release/net9.0/Minesweeper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854d4628df8c2c356f01708f6ffc345bfaddcd89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+118a4ddd626b4069308fff66f06ff812c6d3f84d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
